333afe34b3e2c4e74e383da1874e80da3206fd80
Gorluxor <18031411+Gorluxor@users.noreply.github.com>
2023-05-28 20:27:30 +0300

    rename

--- capp.py
+++ capp.py
@@ -12,9 +12,10 @@ import PIL
 import torch
 from segment_anything import SamAutomaticMaskGenerator, sam_model_registry
 
-CHECKPOINT_PATH = os.path.join(os.path.expanduser("~"), ".cache", "SAM")
+#CHECKPOINT_PATH = os.path.join(os.path.expanduser("~"), ".cache", "SAM")
+CHECKPOINT_PATH = "models"
 CHECKPOINT_NAME = "sam_vit_h_4b8939.pth"
-CHECKPOINT_URL = "https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth"
+#CHECKPOINT_URL = "https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth"
 MODEL_TYPE = "default"
 MAX_WIDTH = MAX_HEIGHT = 1024
 TOP_K_OBJ = 100
@@ -27,6 +28,7 @@ def load_mask_generator() -> SamAutomaticMaskGenerator:
         os.makedirs(CHECKPOINT_PATH)
     checkpoint = os.path.join(CHECKPOINT_PATH, CHECKPOINT_NAME)
     if not os.path.exists(checkpoint):
+        raise ValueError("Please download the checkpoint from the link below and put it in ~/.cache/SAM")
         urllib.request.urlretrieve(CHECKPOINT_URL, checkpoint)
     sam = sam_model_registry[MODEL_TYPE](checkpoint=checkpoint).to(device)
     mask_generator = SamAutomaticMaskGenerator(sam)
@@ -37,7 +39,7 @@ def load_mask_generator() -> SamAutomaticMaskGenerator:
 def load_clip(
     name: str = "ViT-B/32",
 ) -> Tuple[torch.nn.Module, Callable[[PIL.Image.Image], torch.Tensor]]:
-    model, preprocess = clip.load(name, device=device)
+    model, preprocess = clip.load(name, device=device, download_root=CHECKPOINT_PATH)
     return model.to(device), preprocess
 
 
@@ -88,8 +90,68 @@ def crop_image(image: np.ndarray, mask: Dict[str, Any]) -> PIL.Image.Image:
 def get_texts(query: str) -> List[str]:
     return [f"a picture of {query}", "a picture of background"]
 
+@torch.no_grad()
+def embed_text_only(queries: List[str]) -> List[Dict[str, Any]]:
+    model, preprocess = load_clip()
+    tokens = clip.tokenize(queries).to(device)
+    #clip.tokenize(get_texts(queries)).to(device)
+    embs =  model.encode_text(tokens).to(device).cpu()
+    return embs / embs.norm(dim=1, keepdim=True)
+    #  image_features = image_features / image_features.norm(dim=1, keepdim=True)
+    #     text_features = text_features / text_features.norm(dim=1, keepdim=True)
+
+@torch.no_grad()
+def embed_img_only(
+    image: np.ndarray,
+    masks: List[Dict[str, Any]],
+    predicted_iou_threshold: float,
+    stability_score_threshold: float,
+    queries: List[str],
+    clip_threshold: float, 
+) -> List[Dict[str, Any]]:
+    embedded_pictures, crops = [], []
+    model, preprocess = load_clip()
+    for mask in sorted(masks, key=lambda mask: mask["area"])[-TOP_K_OBJ:]:
+        if (
+            mask["predicted_iou"] < predicted_iou_threshold
+            or mask["stability_score"] < stability_score_threshold
+            or image.shape[:2] != mask["segmentation"].shape[:2]
+        ):
+            continue
+        crop = crop_image(image, mask)
+        img_processed = preprocess(crop).unsqueeze(0).to(device)
+        emds = model.encode_image(img_processed)
+        emds = emds / emds.norm(dim=1, keepdim=True)
+        embedded_pictures.append(emds)
+        crops.append(crop)
+
+    return torch.cat(embedded_pictures, dim=0).cpu(), crops
 
 def filter_masks(
+    image: np.ndarray,
+    masks: List[Dict[str, Any]],
+    predicted_iou_threshold: float,
+    stability_score_threshold: float,
+    query: str,
+    clip_threshold: float,
+) -> List[Dict[str, Any]]:
+    filtered_masks: List[Dict[str, Any]] = []
+
+    for mask in sorted(masks, key=lambda mask: mask["area"])[-TOP_K_OBJ:]:
+        if (
+            mask["predicted_iou"] < predicted_iou_threshold
+            or mask["stability_score"] < stability_score_threshold
+            or image.shape[:2] != mask["segmentation"].shape[:2]
+            or query
+            and get_score(crop_image(image, mask), get_texts(query)) < clip_threshold
+        ):
+            continue
+
+        filtered_masks.append(mask)
+
+    return filtered_masks
+
+def filter_masks_multi(
     image: np.ndarray,
     masks: List[Dict[str, Any]],
     predicted_iou_threshold: float,
@@ -134,6 +196,65 @@ def draw_masks(
         cv2.drawContours(image, contours, -1, (0, 0, 255), 2)
     return image
 
+def segment_cluster(
+    predicted_iou_threshold: float,
+    stability_score_threshold: float,
+    clip_threshold: float,
+    image_path: str,
+    query_list: List[str],
+    mask_generator: SamAutomaticMaskGenerator = None,
+    return_masks: bool = False,
+) -> PIL.ImageFile.ImageFile:    
+    if not mask_generator:
+        mask_generator = load_mask_generator()
+    assert isinstance(mask_generator, SamAutomaticMaskGenerator), "mask_generator must be a SamAutomaticMaskGenerator"
+    
+    if isinstance(image_path, str):    
+        image = cv2.imread(image_path, cv2.IMREAD_COLOR)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    elif isinstance(image_path, np.ndarray):
+        image = image_path
+    elif isinstance(image_path, torch.Tensor):
+        image = image_path.numpy()
+    masks = mask_generator.generate(image)
+    
+
+
+def segment_multi(
+    predicted_iou_threshold: float,
+    stability_score_threshold: float,
+    clip_threshold: float,
+    image_path: str,
+    query_list: List[str],
+    mask_generator: SamAutomaticMaskGenerator = None,
+    return_masks: bool = False,
+) -> PIL.ImageFile.ImageFile:
+    
+    if not mask_generator:
+        mask_generator = load_mask_generator()
+    assert isinstance(mask_generator, SamAutomaticMaskGenerator), "mask_generator must be a SamAutomaticMaskGenerator"
+    
+    if isinstance(image_path, str):    
+        image = cv2.imread(image_path, cv2.IMREAD_COLOR)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    elif isinstance(image_path, np.ndarray):
+        image = image_path
+    else:
+        raise ValueError("image_path must be a str or np.ndarray")
+    masks = mask_generator.generate(image)
+    masks = filter_masks_multi(
+        image,
+        masks,
+        predicted_iou_threshold,
+        stability_score_threshold,
+        query_list,
+        clip_threshold,
+    )
+    image = draw_masks(image, masks)
+    image = PIL.Image.fromarray(image)
+    if return_masks:
+        return image, masks
+    return image
 
 def segment(
     predicted_iou_threshold: float,
@@ -141,11 +262,21 @@ def segment(
     clip_threshold: float,
     image_path: str,
     query: str,
+    mask_generator: SamAutomaticMaskGenerator = None,
+    return_masks: bool = False,
 ) -> PIL.ImageFile.ImageFile:
-    mask_generator = load_mask_generator()
-    image = cv2.imread(image_path, cv2.IMREAD_COLOR)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
+    
+    if not mask_generator:
+        mask_generator = load_mask_generator()
+    assert isinstance(mask_generator, SamAutomaticMaskGenerator), "mask_generator must be a SamAutomaticMaskGenerator"
+    
+    if isinstance(image_path, str):    
+        image = cv2.imread(image_path, cv2.IMREAD_COLOR)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    elif isinstance(image_path, np.ndarray):
+        image = image_path
+    else:
+        raise ValueError("image_path must be a str or np.ndarray")
     # reduce the size to save gpu memory
     image = adjust_image_size(image)
     masks = mask_generator.generate(image)
@@ -159,10 +290,15 @@ def segment(
     )
     image = draw_masks(image, masks)
     image = PIL.Image.fromarray(image)
+    if return_masks:
+        return image, masks
     return image
 
 
-demo = gr.Interface(
+
+
+if __name__ == "__main__":
+    demo = gr.Interface(
     fn=segment,
     inputs=[
         gr.Slider(0, 1, value=0.9, label="predicted_iou_threshold"),
@@ -225,7 +361,5 @@ demo = gr.Interface(
             "fish",
         ],
     ],
-)
-
-if __name__ == "__main__":
+    )   
     demo.launch()
